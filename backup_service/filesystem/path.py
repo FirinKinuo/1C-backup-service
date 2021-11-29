1b4718d0f527fee4154e7a80e802ba052a7d7a78
FirinKinuo <deals@fkinuo.ru>
2021-11-29 17:07:10 +0300

    ADD: Filesystem module for search backup files and paths of 1C bases

---
+++ backup_service/filesystem/path.py
@@ -0,0 +1,34 @@
+import re
+import os
+
+from datetime import date, datetime
+
+from pathlib import PosixPath, WindowsPath, Path
+
+
+class BackupPath(PosixPath if os.name == 'posix' else WindowsPath):
+    """Модификация Path от pathlib для работы с путями до бэкап файлов"""
+    def download_path(self) -> Path:
+        """
+        Путь для безопасного распространения в сети
+        Returns:
+            str: Относительный путь до папки и файла с бэкапом backup_dir/backup_file.bak
+        """
+
+        return Path(self.name)
+
+    def backup_date(self) -> date:
+        """
+        Получить дату создания бэкапа
+        Returns:
+            data: Дата создания бэкапа
+        """
+        return datetime.strptime(re.search(r'(\d{8})', self.stem).group(), '%Y%m%d').date()
+
+    def get_size(self) -> int:
+        """
+        Получить размер файла бекапа в МегаБайтах
+        Returns:
+            int: Размер файла
+        """
+        return os.path.getsize(self) * 1024 * 1024
---
+++ backup_service/filesystem/search.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from backup_service.settings import config
+from backup_service.filesystem import path
+
+
+def search_backup_files(base_name: str, backup_month: int) -> list[path.BackupPath]:
+    """
+    Поиск файлов бэкапов переданной базы
+    Args:
+        base_name (str): Название базы данных, для которой необходимо получить файлы бэкапов
+        backup_month (int): Месяц, в котором был создан бэкап
+
+    Returns:
+        list[path.BackupPath]: Список BackupPath до файлов бэкапов
+    """
+    base_backup_folder = Path(config.BACKUP_DIR, base_name)
+
+    if not base_backup_folder.exists():
+        raise FileNotFoundError(f"Бекапы для базы {base_name} по пути {base_backup_folder} не найдены!")
+
+    return list(file for file in map(path.BackupPath, base_backup_folder.rglob(f'*{backup_month}*.bak'))
+                if file.backup_date().month == backup_month)
+
+
+def search_base_backup_folders() -> list[str]:
+    """
+    Поиск папок бэкапов баз
+    Returns:
+        list[str]: Список названий папок с бэкапами
+    """
+    return list(map(lambda backup_path: backup_path.name, config.BACKUP_DIR.iterdir()))
