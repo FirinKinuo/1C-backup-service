7c8277b042a66ab73597a5f73faa06add421d7d5
Firin Kinuo <deals@fkinuo.ru>
2021-12-21 11:10:55 +0300

    Add: MVC to display, downloading backups, user login via LDAP

---
+++ backup_service/web/backups/controllers.py
@@ -0,0 +1,102 @@
+import re
+
+from typing import Union
+from pathlib import Path
+
+from flask import send_file, Response, render_template, g, redirect, request, session
+from flask_simpleldap import LDAP
+
+from backup_service.settings import config
+from backup_service.filesystem import search
+from backup_service.web.backups import models
+
+ldap_manager = LDAP()
+
+
+def response_backup_files(base_name: str, backup_month: int) -> models.BackupResponse:
+    """
+    Получить ответ формата BackupResponse с данными путей до файлов
+    Args:
+        base_name (str): Название базы данных, для которой необходимо найти бэкапы
+        backup_month (int): Месяц, в котором были созданы бэкапы
+
+    Returns:
+        models.BackupResponse: Ответ формата BackupResponse
+    """
+    backup_files = search.search_backup_files(base_name=base_name, backup_month=backup_month)
+    return models.BackupResponse(
+        base_name=base_name,
+        base_name_alias=base_name,
+        files=[models.BackupFile(
+            date=backup.backup_date(),
+            file_url=str(backup.download_path()),
+            size=backup.get_size()) for backup in backup_files]
+    )
+
+
+def response_download_backup(download_file: str) -> Response:
+    """
+    Получить ответ для скачивания файла бэкапа с сервера
+    Args:
+        download_file (str): Имя файла бэкапа
+
+    Returns:
+        Response: Flask Response с файлом бэкапа или ошибку 404
+    """
+    download_path = Path(config.BACKUP_DIR, re.sub(r'(_\d{8}.+)', '', download_file), download_file)
+
+    return send_file(path_or_file=f"{download_path}") if download_path.exists() else Response(status=404)
+
+
+def response_backups_page() -> str:
+    """
+    Получить рендер страницы с бэкапами
+    Returns:
+        str: Строка срендренной страницы
+    """
+    return render_template(
+        'backup_table.html',
+        base_numbers=search.search_base_backup_folders(),
+        title="База бэкапов"
+    )
+
+
+def find_user_ldap_groups():
+    """Поиск LDAP групп, в которых состоит пользователь"""
+    g.user = None
+    if 'user_id' in session:
+        g.user = session['user_id']
+        g.ldap_groups = [group.decode('utf-8') for group in ldap_manager.get_user_groups(user=session['user_id'])]
+
+
+def response_login_page(error_credentials: bool = False) -> Union[str, Response]:
+    """
+    Получить рендер страницы входа или перенаправление на главную страницу, 
+    если вход в аккаунт уже был произведен
+
+    Args:
+        error_credentials (bool): Отправить состояние ошибки в шаблон
+    """
+    return render_template(
+        'login.html',
+        error_credentials=error_credentials) if not g.user else redirect('/')
+
+
+def response_login_user() -> Response:
+    """Авторизация пользователя по LDAP"""
+    ldap_username = request.form['ldap-username']
+    ldap_password = request.form['ldap-password']
+
+    found_user = ldap_manager.bind_user(ldap_username, ldap_password)
+
+    if found_user is not None:
+        session['user_id'] = ldap_username
+        return redirect('/')
+
+    return response_login_page(error_credentials=True)
+
+
+def response_logout_user() -> Response:
+    """Завершение сессии для пользователя"""
+    session.pop('user_id', None)
+    return redirect('/')
--- backup_service/web/backups/views.py
+++ backup_service/web/backups/views.py
@@ -1,20 +1,49 @@
+from typing import Union
+
+from flask_simpleldap import LDAP
 from flask_pydantic import validate
 
-from backup_service.web.backups import blueprint, models, logic
+from backup_service.web.backups import blueprint, models, controllers
+
+
+ldap_manager = LDAP()
+
+
+@blueprint.before_app_request
+def before_request():
+    controllers.find_user_ldap_groups()
+
+
+@blueprint.get('/login')
+def view_login_page() -> str:
+    return controllers.response_login_page()
+
+
+@blueprint.post('/login')
+def login_user() -> Union[str, controllers.Response]:
+    return controllers.response_login_user()
 
 
 @blueprint.get('/')
+@ldap_manager.group_required(groups=['Access_backups'])
 def view_backups() -> str:
-    return logic.response_backups_page()
+    return controllers.response_backups_page()
 
 
 @blueprint.get('/getBackupFiles')
 @validate(query=models.BackupQuery)
+@ldap_manager.group_required(groups=['Access_backups'])
 def view_backup_files(query: models.BackupQuery) -> models.BackupResponse:
-    return logic.response_backup_files(base_name=query.base_name, backup_month=query.month_id)
+    return controllers.response_backup_files(base_name=query.base_name, backup_month=query.month_id)
 
 
 @blueprint.get('/download')
 @validate(query=models.BackupDownloadQuery)
-def download_backup(query: models.BackupDownloadQuery) -> logic.Response:
-    return logic.response_download_backup(download_file=query.backup)
+@ldap_manager.group_required(groups=['Access_backups'])
+def download_backup(query: models.BackupDownloadQuery) -> controllers.Response:
+    return controllers.response_download_backup(download_file=query.backup)
+
+
+@blueprint.get('/logout')
+def logout_user() -> controllers.Response:
+    return controllers.response_logout_user()
