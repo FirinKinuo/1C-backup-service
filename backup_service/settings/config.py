8323a47005fe4ef01f336c20785d0e250cec1e83
Firin Kinuo <deals@fkinuo.ru>
2022-01-12 14:40:50 +0300

    Add: Logger configuration

--- backup_service/settings/config.py
+++ backup_service/settings/config.py
@@ -22,12 +22,14 @@ def get_config_from_yaml() -> dict:
         with open(YAML_CONFIG_PATH, 'r', encoding="utf-8") as config_file:
             return yaml.load(stream=config_file, Loader=yaml.loader.SafeLoader)
     except FileNotFoundError:
-        raise SystemExit(f"Невозможно найти файл конфигурации! Путь: {YAML_CONFIG_PATH}") from SystemExit
+        error_message = f"Невозможно найти файл конфигурации! Путь: {YAML_CONFIG_PATH}"
+        raise SystemExit(error_message) from SystemExit
 
 
 _config = get_config_from_yaml()
 
 DEBUG = bool(_config.get('debug')) or False
+LOGGER_LEVEL = logging.getLevelName(_config.get('log_level').upper())
 FLASK_SECRET_KEY = _config.get('flask_secret_key')
 
 LDAP_TYPE = _config.get('ldap_type')
@@ -45,7 +47,14 @@ LDAP_USER_OBJECT_FILTER = _config.get('ldap_user_object_filter')
 LDAP_GROUP_ACCESS_BACKUP_TABLE = _config.get('ldap_group_access_backup_table') or ''
 LDAP_GROUP_ACCESS_BACKUP_DOWNLOAD = _config.get('ldap_group_access_backup_download') or ''
 LDAP_GROUP_ACCESS_MANAGE_BACKUPS = _config.get('ldap_group_access_manage_backups') or ''
+LDAP_OPENLDAP = _config.get('ldap_openldap') or False
 
 BACKUP_DIR = Path(_config.get('backup_dir')) if _config.get('backup_dir') else None
 
 SQLITE_ENGINE = f"sqlite:///{Path(EXTERNAL_FILES_DIR, _config.get('sqlite_path'))}"
+
+logging.basicConfig(level=LOGGER_LEVEL,
+                    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+                    datefmt="%d/%b/%Y %H:%M:%S",
+                    filename="/var/log/1c-backup-service/backups.log" if not DEBUG else None,
+                    filemode='a')
